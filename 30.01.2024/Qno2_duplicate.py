2a3054ad9b2af890374220b0774ac55772a1bc89
tamilarasu5147 <157339349+tamilarasu5147@users.noreply.github.com>
2024-03-12 22:02:24 +0530

    DAY 10

---
+++ 30.01.2024/Qno2_duplicate.py
@@ -0,0 +1,26 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+'''
+#logic1
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+    
+#logic2
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
